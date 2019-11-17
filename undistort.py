eb95edeb2d0260f54d39ecfedfe448376b3e3aca
Eagle Zhao <tim.eagle.zhao@gmail.com>
2019-11-16 23:35:20 -0500

    done

--- undistort.py
+++ undistort.py
@@ -31,25 +31,47 @@ def undistort_(img_path):
 def undistort(img_path, balance=0.0, dim2=None, dim3=None):
     global cnt
     img = cv2.imread(img_path)
-    dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
-    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
     if not dim2:
-        dim2 = dim1
+        dim2 = DIM
     if not dim3:
-        dim3 = dim1
-    scaled_K = K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
-    scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
-    # This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image. OpenCV document failed to make this clear!
-    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
+        dim3 = dim2
+    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, dim2, np.eye(3), balance=balance)
+    Knew = cv2.omnidir.Matx33f((640*1.5)/3.1415, 0, 0,0, 640/3.1415, 0, 0, 0, 1);
+
+    print new_K,Knew
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), Knew, dim3, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-    #cv2.imshow("undistorted", undistorted_img)
     cv2.imwrite("/home/liquid/proj/pg/"+str(cnt)+".png", undistorted_img)
     cnt+=1
 
-    #if cv2.waitKey(0)& 0xFF == ord('q'):
-    #    quit()
-    #cv2.destroyAllWindows()
+    cv2.namedWindow('undistorted',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('undistorted', 640,720)
+    cv2.imshow("undistorted", undistorted_img)
+
+    if cv2.waitKey(0)& 0xFF == ord('q'):
+        quit()
+    cv2.destroyAllWindows()
+
+def undistort_j(img_path):
+    cam_matrix = np.array([[2.2342347071015597e+03, -3.2044325347697606e+00, 9.4751159627783716e+02], [0,                      2.2237834913412348e+03, 6.4710313963943236e+02],  [0,                      0,                                          1]])
+    dist_coefs = np.array([-3.9194270831617489e-01, 1.2746418822062929e-02, -1.3740618480258631e-03, 5.3496926592307157e-03])
+    xi = np.array([2.1688260313506302])
+
+    '''the undistortion is performed in this way'''
+    img_width = 1920
+    img_height = 1208
+    Knew = K.copy()
+    Knew[0][0] = img_width/2
+    Knew[1][1] = img_width/2
+
+    mapx, mapy = cv2.omnidir.initUndistortRectifyMap(K, D, np.array([1.1]), None, Knew,DIM, cv2.CV_32FC1, cv2.omnidir.RECTIFY_PERSPECTIVE)
+    img = cv2.imread(img_path)
+    img_new = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+
+    cv2.imshow("undistorted", img_new)
+    cv2.imwrite("test_fixed.png", img_new)
+    cv2.waitKey(0)
+
 
 if __name__ == '__main__':
     #for p in sys.argv[1:]:
@@ -57,5 +79,6 @@ if __name__ == '__main__':
     images = glob.glob(sys.argv[1]+'*.png')
 
     for fname in images:
-        #undistort_(fname)
-        undistort(fname,1)
+        undistort_j(fname)
+        undistort(fname,1.0,(640*2,720*2))
+        #undistort(fname,1.0)
